e03a2bc7b03b7b0dcbb50f25ba3dfeda3eeae03e
MD Javed <md.javed@newsignature.com>
2020-05-07 03:32:43 -0400

    Add project files.

---
+++ Seg.Dev.Poc.Sender/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+
+namespace Seg.Dev.Poc.Sender
+{
+    class Program
+    {
+        private const string connectionString = "Endpoint=sb://seg-dev-eh-ns.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=/VBNIG4/+R9SKy8LjbP3f98f64WXFgoSxyzy2RqqHUM=";
+        private const string eventHubName = "seg-dev-poc";
+        static async Main(string[] args)
+        {
+            await using (var producerClient = new EventHubProducerClient(connectionString, eventHubName))
+            {
+                // Create a batch of events 
+                using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+
+                // Add events to the batch. An event is a represented by a collection of bytes and metadata. 
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("First event")));
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Second event")));
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Third event")));
+
+                // Use the producer client to send the batch of events to the event hub
+                await producerClient.SendAsync(eventBatch);
+                Console.WriteLine("A batch of 3 events has been published.");
+            }
+        }
+    }
+}
